3847702c49a1b8ee6b676c158c6720c11415dbac
Tomas Torres <tomas3torres@live.com>
2019-02-10 15:22:15 -0600

    Added functionality

--- lib/scorekeeper.dart
+++ lib/scorekeeper.dart
@@ -6,6 +6,8 @@ class ScoreKeeper extends StatefulWidget {
 }
 
 class ScoreKeeperState extends State<ScoreKeeper> {
+  int itemCount = 2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,34 +24,112 @@ class ScoreKeeperState extends State<ScoreKeeper> {
         child: Column(
           children: <Widget>[
             Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top:30, bottom: 30),
+              margin: EdgeInsets.only(left: 10, right: 10, top:30, bottom: 0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   IconButton(
                     icon: Icon(Icons.settings_backup_restore,color: Colors.white70), 
                     iconSize: 30,
+                    onPressed: reset,
                   ),
                   IconButton(
                     icon: Icon(Icons.add, color: Colors.white70,),
                     iconSize: 30,
+                    onPressed: addPlayer,
                   )
                 ],
               ),
             ),
-            _buildTile(),
-            _buildTile(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: itemCount,
+                itemBuilder: (context, position){
+                  return ListTileItem(position);
+                },
+              ),
+            )
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTile(){
-    return ListTile(
-      title: Text("Test"),
-      leading: Icon(Icons.remove_circle_outline, size: 50, color: Colors.white70,),
-      trailing: Icon(Icons.add_circle_outline, size: 50, color: Colors.white70,),
-    );
+  reset(){
+    setState(() {
+     itemCount = 2; 
+    });
+  }
+
+  addPlayer()
+  {
+    setState(() {
+     itemCount++; 
+    });
   }
 }
+
+class ListTileItem extends StatefulWidget {
+  int index;
+  ListTileItem(int index){
+    this.index = index;
+  }
+  @override
+  ListTileItemState createState() => ListTileItemState(index);
+}
+
+class ListTileItemState extends State<ListTileItem>{
+  int score = 0;
+  int playerNumber;
+
+  ListTileItemState(int index){
+    playerNumber = index+1;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return  ListTile(
+      title: ListTile(
+        title: TextField(
+          style: TextStyle(color: Colors.white70, fontSize: 20),
+          decoration: InputDecoration(
+            hintText: "Player $playerNumber",
+            hintStyle: TextStyle(color: Colors.white70, fontSize: 20),
+            border: InputBorder.none,
+        
+          ),
+        ),
+        trailing: Text(
+          "$score",
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+      leading: IconButton(
+        icon: Icon(Icons.remove_circle_outline, color: Colors.white70),
+        iconSize: 50,
+        color: Colors.white70,
+        onPressed: decScore,
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.add_circle_outline, color: Colors.white70),
+        iconSize: 50,
+        color: Colors.white70,
+        onPressed: incScore,
+      ),
+    );
+  }
+  decScore(){
+    if(score == 0)
+      return;
+    setState(() {
+    score--; 
+    });
+  }
+
+  incScore(){
+    setState(() {
+     score++; 
+    });
+  }
+}
+
